aa25f1185f1d93e8a637f5cc8f921668ae4eb6b0
NhatMinh <ngnhatminh49@gmail.com>
2021-05-26 16:27:24 +0700

    Add output variable

--- main.tf
+++ main.tf
@@ -17,38 +17,42 @@ resource "azurerm_automation_module" "az_aa_module" {
     module_link {
         uri = each.value
     }
+
+    timeouts {
+        create = "15m"
+    }
 }
 
-resource "azurerm_virtual_machine_extension" "az_aa_ext" {
-    name                 = "Microsoft.Powershell.DSC"
-    virtual_machine_id   = var.vm_id
-    publisher            = "Microsoft.Powershell"
-    type                 = "DSC"
-    type_handler_version = "2.77"
-    auto_upgrade_minor_version = true
-    settings = <<SETTINGS_JSON
-            {
-                "configurationArguments": {
-                    "RegistrationUrl" : "${azurerm_automation_account.az_aa.dsc_server_endpoint}",
-                    "ConfigurationMode": "ApplyAndAutoCorrect",
-                    "RefreshFrequencyMins": 30,
-                    "ConfigurationModeFrequencyMins": 15,
-                    "RebootNodeIfNeeded": false,
-                    "ActionAfterReboot": "continueConfiguration",
-                    "AllowModuleOverwrite": false
+# resource "azurerm_virtual_machine_extension" "az_aa_ext" {
+#     name                 = "Microsoft.Powershell.DSC"
+#     virtual_machine_id   = var.vm_id
+#     publisher            = "Microsoft.Powershell"
+#     type                 = "DSC"
+#     type_handler_version = "2.77"
+#     auto_upgrade_minor_version = true
+#     settings = <<SETTINGS_JSON
+#             {
+#                 "configurationArguments": {
+#                     "RegistrationUrl" : "${azurerm_automation_account.az_aa.dsc_server_endpoint}",
+#                     "ConfigurationMode": "ApplyAndAutoCorrect",
+#                     "RefreshFrequencyMins": 30,
+#                     "ConfigurationModeFrequencyMins": 15,
+#                     "RebootNodeIfNeeded": false,
+#                     "ActionAfterReboot": "continueConfiguration",
+#                     "AllowModuleOverwrite": false
     
-                }
-            }
-SETTINGS_JSON
+#                 }
+#             }
+# SETTINGS_JSON
 
-    protected_settings = <<PROTECTED_SETTINGS_JSON
-        {
-            "configurationArguments": {
-                    "RegistrationKey": {
-                        "userName": "NOT_USED",
-                        "Password": "${azurerm_automation_account.az_aa.dsc_primary_access_key}"
-                    }
-            }
-        }
-PROTECTED_SETTINGS_JSON
-}
+#     protected_settings = <<PROTECTED_SETTINGS_JSON
+#         {
+#             "configurationArguments": {
+#                     "RegistrationKey": {
+#                         "userName": "NOT_USED",
+#                         "Password": "${azurerm_automation_account.az_aa.dsc_primary_access_key}"
+#                     }
+#             }
+#         }
+# PROTECTED_SETTINGS_JSON
+# }
